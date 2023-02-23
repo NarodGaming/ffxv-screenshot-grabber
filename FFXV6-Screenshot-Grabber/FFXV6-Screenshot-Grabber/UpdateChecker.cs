0a6cc38f54e5bdc1ea894715ca664f387f1544f7
NarodGaming <arron.faulkner@gmail.com>
2023-02-23 20:45:59 +0000

    Update obsolete WebRequest
    Switches obsolete WebRequest for HTTPClient.

--- FFXV6-Screenshot-Grabber/FFXV6-Screenshot-Grabber/UpdateChecker.cs
+++ FFXV6-Screenshot-Grabber/FFXV6-Screenshot-Grabber/UpdateChecker.cs
@@ -5,27 +5,26 @@ namespace FFXV6_Screenshot_Grabber
     internal static class UpdateChecker
     {
 
-        private static string getLatestVersion()
+        private async static Task<string> getLatestVersion()
         {
             try // try and catch, for in case theres no internet, url is down, etc.
             {
-                WebRequest verRequest = WebRequest.Create("https://ngserve.games/cdn/ffxvsg/latest-ver.txt"); // open a webrequest to the URL storing the version info
-                using (WebResponse response = verRequest.GetResponse()) // get the response of the webrequest, store it in response
+                HttpClient verRequest = new HttpClient(); // create a new httpclient to fetch the latest version
+                HttpResponseMessage verResponse = await verRequest.GetAsync("https://ngserve.games/cdn/ffxvsg/latest-ver.txt"); // get the web page holding the latest ver info
+                if (verResponse.IsSuccessStatusCode) // if we received a success HTTP code
                 {
-                    using (StreamReader reader = new StreamReader(response.GetResponseStream())) // use a streamreader to get the output from the respose
-                    {
-                        return reader.ReadToEnd(); // return the streamreader (which will be version in format Vx.x.x)
-                    }
+                    return await verResponse.Content.ReadAsStringAsync(); // read the web page and return the version
                 }
             } catch (Exception) // if something has gone wrong
             {
                 return "V0.0.0"; // return a 0 version number, so it will never ask the user to update 
             }
+            return "V0.0.0"; // will occur if error HTTP code is returned
         }
 
         public static bool checkForUpdate() // public function called to check for update
         {
-            string latestVersion = getLatestVersion(); // run function to retrieve remote version info
+            string latestVersion = getLatestVersion().GetAwaiter().GetResult(); // run function to retrieve remote version info
             string latestVersionRaw = latestVersion.Substring(1); // remove the "V" at the start. this is in the remote version for potential future use for a better version checker
             latestVersionRaw = latestVersionRaw.Replace(".", ""); // replace the "." with nothing so can be converted to int, this is in the remote version for potential future use for a better version checker
             int latestVersionInt = Convert.ToInt32(latestVersionRaw); // convert the above string to int
